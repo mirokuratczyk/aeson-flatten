c927a6bbb9d94628a706f93eac57cd552845165f
mirokuratczyk <miro.kuratczyk@gmail.com>
2019-04-25 23:02:11 -0400

    Add flatten' which preserves key path

--- src/Data/Aeson/Flatten.hs
+++ src/Data/Aeson/Flatten.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -- |
 -- Module: Data.Aeson.Prefix
 -- Maintainer: Jiri Marsicek <jiri.marsicek@gmail.com>
 --
--- Module provides 'flatten' which is used to flatten structure of 'Data.Aeson' JSON 'Value' as much as possible.
+-- Module provides 'flatten' and 'flatten'' which are used to flatten structure of 'Data.Aeson' JSON 'Value' as much as possible.
 --
--- == Examples
+-- == Examples ('flatten')
 --
 -- === Basic objects
 --
@@ -19,29 +21,48 @@
 -- @{ "a": 1, "b": { "a": 2 } }@ results in @{ "a": 1 }@
 --
 -- * I don't know yet whether this is a feature or a bug :)
+--
+-- == Examples ('flatten'')
+--
+-- === Basic objects
+--
+-- @{ "a": { "b": 1 } }@ results in @{ "a.b": 1 }@
+--
+-- @{ "a": { "b": { "c": 1 } } }@ results in @{ "a.b.c": 1 }@
+--
+-- @{ "a": [ { "b": { "c": 1 } }, { "b": { "c": 2 } } ] }@ results in @{ "a": [ { "b.c": 1 }, { "b.c": 2 } ] }@
+--
+-- === Name conflicts do not result in data loss
+--
+-- @{ "a": 1, "b": { "a": 2 } }@ results in @{ "a": 1, "b.a": 2 }@
 module Data.Aeson.Flatten
     ( flatten
+    , flatten'
     -- * Utility functions
     , isObject
     , mergeTo
+    , tagKeys
     ) where
 
-import Data.Aeson (Value(..))
-import Data.Foldable (foldl')
-import qualified Data.HashMap.Strict as Map (elems, filter, union)
+import           Data.Aeson          (Object, Value (..))
+import           Data.Foldable       (foldl')
+import qualified Data.HashMap.Strict as Map (elems, filter, fromList,
+                                             mapWithKey, toList, union)
+import           Data.Monoid         ((<>))
+import           Data.Text           (Text)
 
 -- |
 -- Predicate for testing whether provided 'Value' is a 'Object'.
 isObject :: Value -> Bool
 isObject (Object _) = True
-isObject _ = False
+isObject _          = False
 
 -- |
 -- Merges keys from second 'Value' to first 'Value'.
 -- In case of name conflict, values from first are preserved.
 mergeTo :: Value -> Value -> Value
 mergeTo (Object o1) (Object o2) = Object $ Map.union o1 o2
-mergeTo o _ = o
+mergeTo o _                     = o
 
 -- |
 -- Recursively Flattens the structure of the provided JSON 'Value'
@@ -49,5 +70,33 @@ flatten :: Value -> Value
 flatten (Array a)  = Array $ flatten <$> a
 flatten (Object o) = let flat = flatten <$> o
                          rest = Map.filter (not . isObject) flat
-                      in foldl' mergeTo (Object rest) $ Map.elems flat
+                     in foldl' mergeTo (Object rest) $ Map.elems flat
 flatten v = v
+
+-- |
+-- Recursively Flattens the structure of the provided JSON 'Value'.
+--
+-- The key of each resulting key/value pair represents the path traversed to reach it in
+-- the initial structure.
+--
+-- E.g. @"{ \"a\": { \"b\": 1 } }"@ results in @"{ \"a.b\": 1 }"@
+flatten' :: Value -> Value
+flatten' = flattenPreserveKeyPath ""
+
+flattenPreserveKeyPath :: Text -> Value -> Value
+flattenPreserveKeyPath k (Array a)  = Array $ flattenPreserveKeyPath k <$> a
+flattenPreserveKeyPath k (Object o) =
+  let flat = Map.mapWithKey (flattenPreserveKeyPath . tagKey k) o
+      rest = tagKeys k $ Map.filter (not . isObject) flat
+  in foldl' mergeTo (Object rest) $ Map.elems flat
+flattenPreserveKeyPath _ v = v
+
+-- |
+-- Prepends text followed by a "." to all keys in the top level of the Map.
+tagKeys :: Text -> Object -> Object
+tagKeys "" o = o
+tagKeys t o  =  Map.fromList $ map (\(k,v) -> (tagKey t k, v)) $ Map.toList o
+
+tagKey :: Text -> Text -> Text
+tagKey "" k = k
+tagKey t k  = t <> "." <> k
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,22 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Test.Hspec
+import           Test.Hspec
 
-import Data.Aeson
-import Data.Aeson.Flatten
-import Data.Maybe (fromJust)
-import Data.ByteString (ByteString)
+import           Data.Aeson
+import           Data.Aeson.Flatten
+import           Data.ByteString    (ByteString)
+import           Data.Maybe         (fromJust)
 
 decodeExample :: ByteString -> Value
 decodeExample = fromJust . decodeStrict'
 
 shouldResultIn :: ByteString -> ByteString -> Expectation
-shouldResultIn input output = flatten (decodeExample input) `shouldBe` flatten (decodeExample output)
+shouldResultIn = shouldResultInFn flatten
+
+shouldResultIn' :: ByteString -> ByteString -> Expectation
+shouldResultIn' = shouldResultInFn flatten'
+
+shouldResultInFn :: (Value -> Value) -> ByteString -> ByteString -> Expectation
+shouldResultInFn flattenFn input output = flattenFn (decodeExample input) `shouldBe` flattenFn (decodeExample output)
 
 -- Test Cases
 
 main :: IO ()
 main = hspec $
-  describe "Data.Aeson.Flatten" $
+  describe "Data.Aeson.Flatten" $ do
     describe "flatten" $ do
       it "works for empty object" $
         "{}" `shouldResultIn` "{}"
@@ -30,3 +36,17 @@ main = hspec $
         "{ \"a\": { \"b\": { \"c\": 1 } } }" `shouldResultIn` "{ \"c\": 1 }"
       it "works for array with one level objects" $
         "{ \"a\": [ { \"b\": { \"c\": 1 } }, { \"b\": { \"c\": 234 } } ] }" `shouldResultIn` "{ \"a\": [ { \"c\": 1 }, { \"c\": 234 } ] }"
+
+    describe "flatten'" $ do
+      it "works for empty object" $
+        "{}" `shouldResultIn'` "{}"
+      it "works for simple object" $
+        "{ \"a\": { \"b\": 1 } }" `shouldResultIn'` "{ \"a.b\": 1 }"
+      it "preserves values where flatten would result in data loss" $
+        "{ \"a\" : { \"b\": 1 }, \"b\": 2 }" `shouldResultIn'` "{ \"a.b\": 1, \"b\": 2 }"
+      it "works for one level flattening" $
+        "{ \"a\": { \"b\": 1, \"c\": 2 } }" `shouldResultIn'` "{ \"a.b\": 1, \"a.c\": 2 }"
+      it "works for two level flattening" $
+        "{ \"a\": { \"b\": { \"c\": 1 } } }" `shouldResultIn'` "{ \"a.b.c\": 1 }"
+      it "works for array with one level objects" $
+        "{ \"a\": [ { \"b\": { \"c\": 1 } }, { \"b\": { \"c\": 234 } } ] }" `shouldResultIn'` "{ \"a\": [ { \"b.c\": 1 }, { \"b.c\": 234 } ] }"
